023e5d3da2fc34ffee38fa8a3f98df2bb8e0a8aa
Tama Waddell <me@tama.id.au>
2022-08-02 20:11:43 +1000

    agent can talk to docker file socket

--- devops/workbook.sh
+++ devops/workbook.sh
@@ -12,8 +12,7 @@
 
 ## local machine (minikube)
 
-
-# create local volumes (node only storage)
+# create directories for local volumes (node only storage)
 minikube ssh
 #$ sudo mkdir -p /var/local-vol
 #$ sudo chown -R 1000:1000 /var/local-vol
@@ -23,11 +22,25 @@ minikube ssh
 minikube start --kubernetes-version=1.23
 
 # deploy 
+kubectl delete -f devops.yaml
 kubectl create -f devops.yaml
 
 # check to see if it's running correctly
+kubectl get sc,pv,pvc
 kubectl get po
 kubectl describe po/docker
+kubectl logs po/docker
+
+kubectl logs po/agent
+
+minikube ssh
+#$sudo curl --unix-socket /var/local-vol/socket/docker.sock http://localhost/images/json
+
+# test connectivity from agent
+kubectl exec -it agent -- /bin/sh
+#$curl --unix-socket /var/socket/docker.sock http://localhost/images/json
+
+
 
 
 
